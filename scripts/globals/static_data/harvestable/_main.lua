b90c9e05d08b3ef89f48d053f39572a054719cc8
Smoozilla <kadehafen@gmail.com>
2024-09-10 09:16:52 -0600

    harvestable data/templates moved to axiom

--- scripts/globals/static_data/harvestable/_main.lua
+++ scripts/globals/static_data/harvestable/_main.lua
@@ -1,7 +1,5 @@
 -- SPDX-License-Identifier: AGPL-3.0-only
 -- Copyright © 2024 Corp Por LTD
 
+-- Used by Harvest effect
 Harvestable = {}
-
-require 'globals.static_data.harvestable.rocks'
-require 'globals.static_data.harvestable.trees'
